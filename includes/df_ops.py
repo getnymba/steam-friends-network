4cbaa89ca7cae0f90b786b2b005a814ef5e1a82b
Nyambayar Demberelsuren <59921058+getnymba@users.noreply.github.com>
2024-02-11 21:13:14 +0800

    fix: faster df apply

--- includes/df_ops.py
+++ includes/df_ops.py
@@ -1,28 +1,17 @@
-import pandas as pd
-import ast
-
-
 class DataframeOperations:
     def add_data_to_df(self, df, data):
-        df.loc[len(df)] = data
+        if data[0] not in df["id"].values:
+            df.loc[len(df)] = data
 
-    def create_source_target_df(self, orig_df):
-        st_df = pd.DataFrame(columns=["source", "target"])
-        for id in orig_df["id"]:
-            friends = ast.literal_eval(
-                orig_df[orig_df["id"] == id]["friends"].values[0]
-            )
-            for friend in friends:
-                if (
-                    st_df[
-                        ((st_df["source"] == id) & (st_df["target"] == friend[0]))
-                        | ((st_df["target"] == id) & (st_df["source"] == friend[0]))
-                    ].shape[0]
-                    == 0
-                ):
-                    self.add_data_to_df(st_df, [id, friend[0]])
+    def create_source_target_df(self, df):
+        df["friends"] = df["friends"].apply(eval)
 
-        return st_df
+        # Use explode to transform the lists into separate rows
+        result_df = df.explode("friends")
+
+        # Rename columns to match your desired output
+        result_df.columns = ["source", "target"]
+        return result_df
 
     def create_attribute_df(self, orig_df):
         return orig_df.drop(columns=["friends"])
