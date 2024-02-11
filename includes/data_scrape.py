c08170601a6e64e13423995a15bd3eb9d476bb15
Nyambayar Demberelsuren <59921058+getnymba@users.noreply.github.com>
2024-02-11 21:12:58 +0800

    fix: get data by scraping instead of Steam API

--- includes/data_scrape.py
+++ includes/data_scrape.py
@@ -1,52 +1,26 @@
 import aiohttp
 from bs4 import BeautifulSoup
-import re
 
 
 class Scraper:
-    async def crawl_friends_from_id(self, id: int) -> list:
+    async def crawl_friends_from_id(self, url) -> list:
         async with aiohttp.ClientSession() as session:
-            async with session.get(
-                f"https://steamcommunity.com/profiles/{id}/friends"
-            ) as response:
+            async with session.get(url) as response:
                 html = await response.text()
-
         soup = BeautifulSoup(html, "html.parser")
         friends = []
 
-        elements = soup.find_all(attrs={"data-steamid": True, "data-search": True})
-
+        elements = soup.find_all("div", class_="friendBlock")
         for element in elements:
-            friends.append((element["data-steamid"], element["data-search"]))
+            user_id = element.find("a", class_="friendBlockLinkOverlay")["href"]
+            friends.append(user_id)
 
         return friends
 
-    async def crawl_country_from_id(self, id: int) -> str:
-        async with aiohttp.ClientSession() as session:
-            async with session.get(
-                f"https://steamcommunity.com/profiles/{id}"
-            ) as response:
-                html = await response.text()
 
-        soup = BeautifulSoup(html, "html.parser")
-        profile_flag_element = soup.find("img", class_="profile_flag")
+if __name__ == "__main__":
+    import asyncio
 
-        if profile_flag_element:
-            profile_flag_value = profile_flag_element.find_next_sibling(text=True)
-            return profile_flag_value.strip() if profile_flag_value else None
-        else:
-            return None
-
-    async def crawl_steamid_from_profile(self, profile_url) -> int:
-        async with aiohttp.ClientSession() as session:
-            async with session.get(profile_url) as response:
-                html = await response.text()
-
-        pattern = r'"steamid":"(\d+)"'
-        match = re.search(pattern, html)
-
-        if match:
-            steamid = match.group(1)
-            return int(steamid)
-        else:
-            return None
+    scraper = Scraper()
+    result = asyncio.run(scraper.crawl_friends_from_id(76561198023455525))
+    print(result)
